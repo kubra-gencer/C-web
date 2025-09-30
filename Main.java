6c311c76bcc68abaf6f5f78313688deec42e4c87
kubra-gencer <kubranur.gencer@bahcesehir.edu.tr>
2025-09-30 22:26:40 +0300

    Create Main.java
    practice

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+
+  Inventory inventory = new Inventory();
+  
+  Item item1 = new Item(name: “Apples“, quantity:20);
+  Item item2 = new Item(name: “Sword“, quantity:2);
+inventory.addItem(item1);
+inventory.addItem(item2);
+
+inventory.displayInventory();
+}
